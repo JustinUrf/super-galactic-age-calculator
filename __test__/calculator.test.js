45953096ef0e83faed1f7d066a5ce619c8035cca
Justin Lee <lee.justin001126@gmail.com>
2023-02-03 15:25:05 -0800

    add new test for GalacticAge() function that fetches the age for all planets

--- __test__/calculator.test.js
+++ __test__/calculator.test.js
@@ -31,19 +31,33 @@ describe('GalacticAge', () => {
     expect(userAge.jupiterAge).toEqual("0.59");
     console.debug(userAge);
   })
+
+  test("Should test the updated function milkywayAge() to return users age of all planets in the solar system to the nearest rounded tenth", () => {
+    const userAge = new GalacticAge(7);
+    userAge.milkywayAge();
+    expect(userAge.mercuryAge).toEqual(parseFloat(userAge.userAge/.24).toFixed(2));
+    expect(userAge.venusAge).toEqual(parseFloat(userAge.userAge/.62).toFixed(2));
+    expect(userAge.marsAge).toEqual(parseFloat(userAge.userAge/1.88).toFixed(2));
+    expect(userAge.jupiterAge).toEqual(parseFloat(userAge.userAge/11.86).toFixed(2));
+    expect(userAge.saturnAge).toEqual(parseFloat(userAge.userAge/29.48).toFixed(2));
+    expect(userAge.uranusAge).toEqual(parseFloat(userAge.userAge/84.07).toFixed(2));
+    expect(userAge.neptuneAge).toEqual(parseFloat(userAge.userAge/164.90).toFixed(2));
+
+  })
+
   
   test("Should test the class function amountOfYearsPassed to return the amount of time passed", () => {
     const userAge = new GalacticAge(18);
     let results = userAge.yearsPassed(11);
-    console.debug(results);
-    expect(results.length).toEqual(5 || 1);
+    // console.debug(results);
+    expect(results.length).toEqual(6 || 1);
   })
 
   test("Should test the class function yearsTillNextBirthday to return the amount of time that needs to pass till a given birthday e.x > 13 earth years need to pass for your 66th birthday", () => {
     const userAge = new GalacticAge(18);
     let yearsPassed = userAge.yearsTillNextBirthday(36);
-    console.debug(yearsPassed);
-    expect(yearsPassed);
+    // console.debug(yearsPassed);
+    // expect(yearsPassed);
     expect(yearsPassed.length).toEqual(5 || 1);
   })
 })

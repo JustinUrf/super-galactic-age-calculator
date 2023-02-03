81c982de2bfbe7771457d6288f2b921592a59333
Justin Lee <lee.justin001126@gmail.com>
2023-02-03 10:08:41 -0800

    add js logic to calculator user's venusAge

--- src/calculator.js
+++ src/calculator.js
@@ -1,10 +1,14 @@
 export default class GalacticAge {
   constructor(userAge) {
-    this.userAge = userAge
+    this.userAge = userAge;
   }
   mercuryAge() {
     let mercuryAge = this.userAge / .24;
-    this.mercuryAge = parse.Float(mercuryAge).toFixed(2);
+    this.mercuryAge = parseFloat(mercuryAge).toFixed(2);
+  }
+  venusAge() {
+    let venusAge = this.userAge / .62;
+    this.venusAge = parseFloat(venusAge).toFixed(2);
   }
 
 

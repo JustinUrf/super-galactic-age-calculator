6407238b55f1c1eec45387ccde52bae78d45a3ef
Justin Lee <lee.justin001126@gmail.com>
2023-02-03 09:45:39 -0800

    red light factor for mercuryAge method

--- __test__/calculator.test.js
+++ __test__/calculator.test.js
@@ -5,6 +5,12 @@ describe('GalacticAge', () => {
   test("Should return user age which will then be used to calculate everything else.", () => {
     const userAge = new GalacticAge(7);
     expect(userAge.userAge).toEqual(7);
+  })
+
+  test("Should test class function mercuryAge() to return user's mercury age ",() => {
+    const userAge = new GalacticAge(7)
+    userAge.mercuryAge();
+    expect(userAge.mercuryAge).toEqual(29.17)
 
   })
 })
--- src/calculator.js
+++ src/calculator.js
@@ -2,5 +2,9 @@ export default class GalacticAge {
   constructor(userAge) {
     this.userAge = userAge
   }
+  mercuryAge() {
+    
+  }
+
 
 }
